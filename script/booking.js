3ffa1da690c3adf7e99181beaf66b4c2712fce2e
Onur <ob-vest@hotmail.com>
2022-05-30 22:36:17 +0200

    dynamic text done

--- script/booking.js
+++ script/booking.js
@@ -27,17 +27,37 @@ function addParagraphToDescription(text) {
 
 function getOption(optionText) {
     switch (optionText) {
-        case ("stortServiceHardtail") || ("stortServiceFullSuspension"):
+        case ("lilleService"):
+            return option = {
+                title: "Stort Service (Hardtail)",
+                descriptions: ["Grundig vask af cykel", "Gennemgang af cykel", "Opspænding af alle bolte", "Opretning af hjul", "Justering af gear", "Justering af bremser", "Smørelse af bevægelige dele"]
+            }
+            break
+        case ("stortServiceHardtail"):
             return option = {
                 title: "Stort Service (Hardtail)",
                 descriptions: ["Grundig vask af cykel", "Komplet adskillelse af cykel", "Rengøring/opsmøring af dele", "Opretning af hjul", "Samling samt justering", "Bike Protect fra Muc-Off"]
             }
-            break;
-        // case ("stortServiceFullSuspension"):
-        //     return option = {
-        //         title: "Stort Service (Full Suspension)",
-        //         descriptions: ["hello", "farewell"]
-            // }
+            break
+        case ("stortServiceFullSuspension"):
+            return option = {
+                title: "Stort Service (Full Suspension)",
+                descriptions: ["Grundig vask af cykel", "Komplet adskillelse af cykel", "Rengøring/opsmøring af dele", "Opretning af hjul", "Samling samt justering", "Bike Protect fra Muc-Off"]
+            }
+            break
+            case ("kompletServiceHardtail"):
+                return option = {
+                    title: "Stort Service (Full Suspension)",
+                    descriptions: ["Stort Service Plus", "Air Can Service", "Lower Leg Service", "Udskiftning af gearkabler", "Opsætning af forgaffel", "Opsætning af bagdæmper"]
+                }
+                break
+            case ("kompletServiceFullSuspension"):
+                return option = {
+                    title: "Stort Service (Full Suspension)",
+                    descriptions: ["Stort Service Plus", "Air Can Service", "Lower Leg Service", "Udskiftning af gearkabler", "Opsætning af forgaffel", "Opsætning af bagdæmper"]
+                }
+                break
+     
     }
 
 }
